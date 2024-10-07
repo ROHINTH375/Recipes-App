ce5844b402c4e590392e0abf0765208edf0a1240
Rohinth <rohinth1904@gmail.com>
2024-10-07 10:12:37 +0530

    first commit

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const RecipeSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  name: { type: String, required: true },
   ingredients: { type: String, required: true },
-  ingredients: { type: String, required: true },
+ /* ingredients: { type: String, required: true },*/
   instructions: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
